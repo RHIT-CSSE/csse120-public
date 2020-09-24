ffe1c7530e63a7dff7ed853bdf2894f71d29e7da
David Mutchler <mutchler@rose-hulman.edu>
2020-09-24 15:57:41 -0400

    Updated authors to include this term's instructors. Renumbered to reflect this term's schedule. Put most recent version of rosegraphics.

--- PythonProjects/12-MoreSequences/src/m1_pizza.py
+++ PythonProjects/12-MoreSequences/src/m1_pizza.py
@@ -5,9 +5,9 @@ This module lets you practice:
   -- DEFINING functions
   -- CALLING functions
 
-Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Mark Hays, Amanda Stouder, Derek Whitley, their colleagues,
-         and PUT_YOUR_NAME_HERE.
+Authors: David Mutchler, Sana Ebrahimi, Mohammed Noureddine, Vibha Alangar,
+         Matt Boutell, Dave Fisher, Mark Hays, their colleagues, and
+         PUT_YOUR_NAME_HERE.
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
