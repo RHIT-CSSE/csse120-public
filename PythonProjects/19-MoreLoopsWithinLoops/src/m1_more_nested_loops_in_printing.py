707947727b9e8d89f3c7c982ce5b8c17e6131c6e
David Mutchler <mutchler@rose-hulman.edu>
2020-10-19 17:32:22 -0400

    Renumbered for this term, authors for this term, current version of rosegraphics.

--- PythonProjects/19-MoreLoopsWithinLoops/src/m1_more_nested_loops_in_printing.py
+++ PythonProjects/19-MoreLoopsWithinLoops/src/m1_more_nested_loops_in_printing.py
@@ -2,9 +2,9 @@
 This project lets you practice NESTED LOOPS (i.e., loops within loops)
 in the context of PRINTING on the CONSOLE.
 
-Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Mark Hays, Amanda Stouder, Derek Whitley, their colleagues,
-         and PUT_YOUR_NAME_HERE.
+Authors: David Mutchler, Sana Ebrahimi, Mohammed Noureddine, Vibha Alangar,
+         Matt Boutell, Dave Fisher, Mark Hays, their colleagues, and
+         PUT_YOUR_NAME_HERE.
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
