6b639dbda54d4f68e68adc357c162eec344be7ca
David Mutchler <mutchler@rose-hulman.edu>
2021-03-20 15:55:45 -0400

    Updated the instructors for this term.

--- PythonProjects/05b-Exam1Practice/src/problem0.py
+++ PythonProjects/05b-Exam1Practice/src/problem0.py
@@ -7,8 +7,8 @@ These problems illustrate concepts that previous problems have not emphasized:
   -- is_prime (Problem 0b)
   -- animation (Problem 0c)
 
-Authors: David Mutchler, Sana Ebrahimi, Sriram Mohan, Mohammed Noureddine,
-         Vibha Alangar, Matt Boutell, Dave Fisher, their colleagues, and
+Authors: David Mutchler, Vibha Alangar, Dave Fisher, Matt Boutell, Mark Hays,
+         Mohammed Noureddine, Sana Ebrahimi, Sriram Mohan, their colleagues and
          PUT_YOUR_NAME_HERE.
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
--- PythonProjects/05b-Exam1Practice/src/problem2.py
+++ PythonProjects/05b-Exam1Practice/src/problem2.py
@@ -1,8 +1,8 @@
 """
 PRACTICE Exam 1, problem 2.
 
-Authors: David Mutchler, Sana Ebrahimi, Sriram Mohan, Mohammed Noureddine,
-         Vibha Alangar, Matt Boutell, Dave Fisher, their colleagues, and
+Authors: David Mutchler, Vibha Alangar, Dave Fisher, Matt Boutell, Mark Hays,
+         Mohammed Noureddine, Sana Ebrahimi, Sriram Mohan, their colleagues and
          PUT_YOUR_NAME_HERE.
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
