0fa5bc242cd10a50672b89ba39b27a7d985badf2
David Mutchler <mutchler@rose-hulman.edu>
2021-03-21 00:16:19 -0400

    Added difficulty and time ratings.

--- PythonProjects/05b-Exam1Practice/src/problem4.py
+++ PythonProjects/05b-Exam1Practice/src/problem4.py
@@ -7,6 +7,27 @@ Authors: David Mutchler, Vibha Alangar, Dave Fisher, Matt Boutell, Mark Hays,
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
+###############################################################################
+# Students:
+#
+# These problems have DIFFICULTY and TIME ratings:
+#  DIFFICULTY rating:  1 to 10, where:
+#     1 is very easy
+#     3 is an "easy" Test 1 question.
+#     5 is a "typical" Test 1 question.
+#     7 is a "hard" Test 1 question.
+#    10 is an EXTREMELY hard problem (too hard for a Test 1 question)
+#
+#  TIME ratings: A ROUGH estimate of the number of minutes that we
+#     would expect a well-prepared student to take on the problem.
+#
+#  IMPORTANT: For ALL the problems in this module,
+#    if you reach the time estimate and are NOT close to a solution,
+#    STOP working on that problem and ASK YOUR INSTRUCTOR FOR HELP
+#    on it, in class or via Piazza.
+###############################################################################
+
+
 def main():
     """ Calls the   TEST   functions in this module. """
     test_factor_sum()
@@ -15,26 +36,30 @@ def main():
 def test_factor_sum():
     """ Tests the   factor_sum   function. """
     ###########################################################################
-    #  TODO: 2. Implement this TEST function, as follows:
-    #   _
-    #    1. Read the  doc-string of the   factor_sum   function defined below.
-    #   _
-    #    2. Add to THIS function at least ** 5 ** tests that, taken together,
-    #       would form a    ** REASONABLY GOOD test set **
-    #       for testing the   factor_sum   function defined below.
-    #   _
-    #     Use the usual format:
-    #       Test XXX:
-    #       expected = XXX
-    #       actual = XXX
-    #       print()
-    #       print('Expected:', expected)
-    #       print('Actual:  ', actual)
-    #   _
-    #   IMPORTANT:
-    #   The function that you are TESTING is PURPOSELY implemented INCORRECTLY
-    #   (it just returns 0).  Do NOT implement the   factor_sum   function.
-    #   Just write these TESTS of that function after reading its doc-string.
+    # TODO: 2. Implement this TEST function, as follows:
+    #  _
+    #   1. Read the  doc-string of the   factor_sum   function defined below.
+    #  _
+    #   2. Add to THIS function at least ** 5 ** tests that, taken together,
+    #      would form a    ** REASONABLY GOOD test set **
+    #      for testing the   factor_sum   function defined below.
+    #  _
+    #    Use the usual format:
+    #      Test XXX:
+    #      expected = XXX
+    #      actual = XXX
+    #      print()
+    #      print('Expected:', expected)
+    #      print('Actual:  ', actual)
+    #  _
+    #  IMPORTANT:
+    #  The function that you are TESTING is PURPOSELY implemented INCORRECTLY
+    #  (it just returns 0).  Do NOT implement the   factor_sum   function.
+    #  Just write these TESTS of that function after reading its doc-string.
+    #  ------------------------------------------------------------------------
+    #  DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
+    #    DIFFICULTY:      3
+    #    TIME ESTIMATE:   10 minutes.
     ###########################################################################
     print()
     print('---------------------------------------------------------')
