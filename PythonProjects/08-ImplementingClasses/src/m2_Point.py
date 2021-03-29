d4ea086fa70e23da7798d09b3f4b479999946cff
David Mutchler <mutchler@rose-hulman.edu>
2021-03-29 12:16:35 -0400

    Renumbering for this term. Instructors for this term.  Current version of testing_helper.py

--- PythonProjects/08-ImplementingClasses/src/m2_Point.py
+++ PythonProjects/08-ImplementingClasses/src/m2_Point.py
@@ -2,8 +2,8 @@
 A simple   Point   class.
 NOTE: This is NOT rosegraphics -- it is your OWN Point class.
 
-Authors: David Mutchler, Sana Ebrahimi, Sriram Mohan, Mohammed Noureddine,
-         Vibha Alangar, Matt Boutell, Dave Fisher, their colleagues, and
+Authors: David Mutchler, Vibha Alangar, Dave Fisher, Matt Boutell, Mark Hays,
+         Mohammed Noureddine, Sana Ebrahimi, Sriram Mohan, their colleagues and
          PUT_YOUR_NAME_HERE.
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
@@ -24,6 +24,7 @@ def main():
     run_test_get_distance_traveled()
     run_test_closer_to()
     run_test_halfway_to()
+
 
 ###############################################################################
 # IMPORTANT:
