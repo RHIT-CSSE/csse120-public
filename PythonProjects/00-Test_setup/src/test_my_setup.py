8026ffaed5a17c8f971467d9de1089b9d871bed8
David Mutchler <mutchler@rose-hulman.edu>
2021-11-28 00:31:35 -0500

    Broke a PRINT across two lines to better suit putting the RUN window on the RHS.

--- PythonProjects/00-Test_setup/src/test_my_setup.py
+++ PythonProjects/00-Test_setup/src/test_my_setup.py
@@ -4,4 +4,5 @@ print("  1. Right-click in this window and")
 print("  2. Select:    Run 'test_my_setup'")
 
 print()
-print("If this message shows up in a window, your Python setup is OK.")
+print("If this message shows up in a window,")
+print("your Python setup is OK.")
