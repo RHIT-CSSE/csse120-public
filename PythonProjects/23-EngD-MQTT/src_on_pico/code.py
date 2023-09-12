a48bbbf6c9fd76c514e533334000337536322e16
David Mutchler <mutchler@rose-hulman.edu>
2023-09-12 18:05:25 -0400

    Updated for this term.

--- PythonProjects/23-EngD-MQTT/src_on_pico/code.py
+++ PythonProjects/23-EngD-MQTT/src_on_pico/code.py
@@ -1,82 +1,85 @@
-# Example showing Pico communicating with another device through MQTT.
-# Author:  David Mutchler, Rose-Hulman Institute of Technology.
+# Example showing a Pico, running CircuitPython,
+# communicating with another device through MQTT.
+# Author:  David Mutchler, Rose-Hulman Institute of Technology,
+#          based on examples from the internet.
 
-import time
+# These imports are for the WIFI and MQTT communication:
+import ssl
+import socketpool
+import wifi
+import adafruit_minimqtt.adafruit_minimqtt as MQTT
+
+# These imports are for simulating sending sensor data:
 import random
-import board
-import busio
-import adafruit_requests as requests
-import adafruit_esp32spi.adafruit_esp32spi_socket as adafruit_socket
-import adafruit_minimqtt.adafruit_minimqtt as adafruit_MQTT
-
-from digitalio import DigitalInOut
-from adafruit_esp32spi import adafruit_esp32spi
-
-# Load the WiFi and HiveMQ Cloud credentials from secrets.py
-from secrets import secrets
+import time
 
 # MQTT Topics to publish/subscribe from/to Pico to/from HiveMQ Cloud
 UNIQUE_ID = "DavidMutchler1019"  # Use something that no one else will use
 PC_TO_DEVICE_TOPIC = UNIQUE_ID + "/pc_to_device"
 DEVICE_TO_PC_TOPIC = UNIQUE_ID + "/device_to_pc"
 
-# Initialize the Pico pins and WiFi module.
-esp32_cs = DigitalInOut(board.GP13)
-esp32_ready = DigitalInOut(board.GP14)
-esp32_reset = DigitalInOut(board.GP15)
-spi = busio.SPI(board.GP10, board.GP11, board.GP12)
-esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
-
-# Handle HTTP requests
-requests.set_socket(adafruit_socket, esp)
-
-# Check ESP32 status
-print("Checking ESP32")
-if esp.status == adafruit_esp32spi.WL_IDLE_STATUS:
-    print("\tESP32 found and in idle mode")
-print("\tFirmware version: ", esp.firmware_version)
-print("\tMAC address: ", [hex(i) for i in esp.MAC_address])
-
-# List the detected WiFi networks
-print("Discovered WiFi networks:")
-for ap in esp.scan_networks():
-    print("\t", (str(ap["ssid"], "utf-8")), "\t\tRSSI: ", ap["rssi"])
+# Load the WiFi and HiveMQ Cloud credentials from the file: secrets.py
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
 
 # Connect to the configured WiFi network
-print("Connecting to WiFi: ", secrets["ssid"])
-while not esp.is_connected:
-    try:
-        esp.connect_AP(secrets["ssid"], secrets["password"])
-    except RuntimeError as e:
-        print("\tCould not connect to WiFi: ", e)
-        continue
-print("\tConnected to ", str(esp.ssid, "utf-8"), "\t\tRSSI:", esp.rssi)
-print("\tIP address of this board: ", esp.pretty_ip(esp.ip_address))
-print("\tPing google.com: " + str(esp.ping("google.com")) + "ms")
+print("\n\nAttempting to connect to WiFi: ", secrets["ssid"], "...")
+wifi.radio.connect(secrets["ssid"], secrets["password"])
 
-# Configure MQTT to use the ESP32 interface
-adafruit_MQTT.set_socket(adafruit_socket, esp)
+print("\tConnected to %s!\n" % secrets["ssid"])
 
-# Configure MQTT client.
-mqtt_client = adafruit_MQTT.MQTT(
+
+# Define callback methods and assign them to the MQTT events
+def on_message(mqtt_client, userdata, message):
+    print("\tReceived a message:", message)
+
+
+def on_connect(client, userdata, flags, rc):
+    print("\tConnected to MQTT broker: ", client.broker, "\n")
+
+
+def on_disconnect(mqtt_client, userdata, rc):
+    print("Disconnected from MQTT Broker!")
+
+
+def on_subscribe(mqtt_client, userdata, topic, granted_qos):
+    print("Subscribed to {0} with QOS level {1}".format(topic, granted_qos))
+
+
+def on_unsubscribe(mqtt_client, userdata, topic, pid):
+    print("Unsubscribed from {0} with PID {1}".format(topic, pid))
+
+
+def on_publish(mqtt_client, userdata, topic, pid):
+    print("Published to {0} with PID {1}".format(topic, pid))
+
+
+# Create a socket pool
+pool = socketpool.SocketPool(wifi.radio)
+
+# Set up a MiniMQTT Client
+mqtt_client = MQTT.MQTT(
     broker=secrets["broker"],
     port=secrets["port"],
     username=secrets["mqtt_username"],
     password=secrets["mqtt_key"],
+    socket_pool=pool,
+    ssl_context=ssl.create_default_context(),
 )
 
-# Define callback methods and assign them to the MQTT events
-def connected(client, userdata, flags, rc):
-    print("\tConnected to MQTT broker: ", client.broker)
-
-def on_message(mqtt_client, userdata, message):
-    print("\tReceived a message:", message)
-
-mqtt_client.on_connect = connected
+# Connect callback handlers to mqtt_client
+mqtt_client.on_connect = on_connect
 mqtt_client.on_message = on_message
+mqtt_client.on_disconnect = on_disconnect
+mqtt_client.on_subscribe = on_subscribe
+mqtt_client.on_unsubscribe = on_unsubscribe
+# mqtt_client.on_publish = on_publish
 
 # Connect to the MQTT broker
-print("Connecting to MQTT broker...")
+print("Attempting to connect to %s ..." % mqtt_client.broker)
 try:
     mqtt_client.connect()
     mqtt_client.subscribe(PC_TO_DEVICE_TOPIC)
@@ -85,11 +88,12 @@ except Exception as e:
 
 # Simulate sending sensor data to the Broker by sending random numbers.
 counter = 0
+loop_counter = 0
 while True:
     mqtt_client.loop()  # Poll for about 1 second to see if any messages have arrived
 
     # Send a message (simulating sending sensor data):
-    if counter >= 3:  # Send (publish) every 3 times through this loop
+    if counter >= 10:  # Send (publish) every 10 times through this loop
         counter = 0
         simulated_sensor_data = random.randint(1, 100)  # Simulate sensor data
         message_to_send = str(simulated_sensor_data)
@@ -98,3 +102,12 @@ while True:
 
     time.sleep(0.1)  # Sleep a bit to safeguard against inundating the message-passing
     counter = counter + 1
+    loop_counter = loop_counter + 1
+#    if loop_counter > 300:
+#        break
+
+print("Unsubscribing from %s" % PC_TO_DEVICE_TOPIC)
+mqtt_client.unsubscribe(PC_TO_DEVICE_TOPIC)
+
+print("Disconnecting from %s" % mqtt_client.broker)
+mqtt_client.disconnect()
